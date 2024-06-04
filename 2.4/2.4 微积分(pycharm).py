585b2dc4b381ab9315d2952a356004caf801670e
handsomekeai <lzllzlo@sina.com>
2024-06-04 09:39:30 +0800

    2.4.1

--- 2.4/2.4 微积分(pycharm).py
+++ 2.4/2.4 微积分(pycharm).py
@@ -1,12 +1,5 @@
-# %matplotlib-inline
 import numpy as np
-
-# form matplotlib_inline import backend_inline
-import backend
-import matplotlib_inline
 import matplotlib.pyplot as plt
-
-# form d2l import torch as d2l
 import torch
 import d2l
 
@@ -22,14 +15,15 @@ for i in range(5):
     print(f'h={h:.5f}, numerical limit={numerical_lim(f, 1, h):.5f}')
     h *= 0.1
 
-def use_svg_dispaly():  #@save
+def use_svg_display():  #@save
     """使用svg格式在Jupyter中显示绘图"""
-    backend_inline.set_matplotlib_formats('svg')
+    plt.rcParams['figure.figsize'] = [3.5, 2.5]
+    plt.rcParams['figure.dpi'] = 100
 
 def set_figsize(figsize=(3.5, 2.5)): #@save
     """设置matplotlib的图表大小"""
-    use_svg_dispaly()
-    d2l.plt.rcParams['figure.figsize'] = figsize
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
 
 #@save
 def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
@@ -47,13 +41,13 @@ def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
 #@save
 def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None,
          ylim=None, xscale='linear', yscale='linear',
-         fmts=('-', 'm--', 'g-', 'r:'), figsize=(3.5, 2.5),axes=None):
+         fmts=('-', 'm--', 'g-', 'r:'), figsize=(3.5, 2.5), axes=None):
     """绘制数据点"""
     if legend is None:
         legend = []
 
     set_figsize(figsize)
-    axes = axes if axes else d2l.plt.gca()
+    axes = axes if axes else plt.gca()
 
     # 如果X有一个轴输出True
     def has_one_axis(X):
@@ -65,19 +59,20 @@ def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None,
     if Y is None:
         X, Y = [[]] * len(X), X
     elif has_one_axis(Y):
-        X = X * len(Y)
-    if len(X) !=len(Y):
+        Y = [Y]
+    if len(X) != len(Y):
         X = X * len(Y)
     axes.cla()
     for x, y, fmt in zip(X, Y, fmts):
         if len(x):
             axes.plot(x, y, fmt)
         else:
-            axes.plot(y,fmt)
+            axes.plot(y, fmt)
     set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
 
-    x = np.arange(0, 3, 0.1)
-    plot(x, [f(x), 2 * x - 3], 'x', 'f(x)', legend=['f(x)', 'Tangent line (x=1)'])
+# 示例绘图
+x = np.arange(0, 3, 0.1)
+plot(x, [f(x), 2 * x - 3], 'x', 'f(x)', legend=['f(x)', 'Tangent line (x=1)'])
 
 # 显示绘图
 plt.show()
