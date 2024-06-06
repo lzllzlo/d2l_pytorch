2849e169a7f655320d3cdcc38787a8aaf0d2d2b2
handsomekeai <lzllzlo@sina.com>
2024-06-06 17:14:25 +0800

    2.5.4

--- 2.5/2.5 自动微分.py
+++ 2.5/2.5 自动微分.py
@@ -33,3 +33,51 @@ print('y=', y)
 # 等价于y.backward(torch.ones(len(x)))
 y.sum().backward()
 print('x.grad=', x.grad)
+
+# 2.5.3 分离结算
+print('\n')
+print(x.grad)
+x.grad.zero_()
+print(x.grad)
+print(y)
+y = x * x
+print(y)
+u = y.detach()
+print(u)
+print(x)
+z = u * x
+print(z)
+
+z.sum().backward()
+print(x.grad == u)
+
+x.grad.zero_()
+y.sum().backward()
+print(x)
+print(x.grad)
+print(x.grad == 2 * x)
+
+
+# 2.5.4 Python控制流的梯度计算
+print('2.5.4')
+
+
+def f(a):
+    b = a * 2
+    while b.norm() < 1000:
+        print('b=', b)
+        print('b.norm=', b.norm)
+        print('b.sum=', b.sum)
+        b = b * 2
+    if b.sum() > 0:
+        c = b
+    else:
+        c = 100 * b
+    return c
+
+
+a = torch.randn(size=(), requires_grad=True)
+d = f(a)
+d.backward()
+print(a.grad)
+print(a.grad == d / a)
