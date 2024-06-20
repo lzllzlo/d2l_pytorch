4f1f416f122db8ff441fb47546f8b68f891a42df
handsomekeai <lzllzlo@sina.com>
2024-06-20 11:58:32 +0800

    3.2.3

--- 第三章/3.2.py
+++ 第三章/3.2.py
@@ -21,11 +21,12 @@ print('features:', features[999], '\nlabel:', labels[999])
 d2l.set_figsize()
 d2l.plt.scatter(features[:, (1)].detach().numpy(), labels.detach().numpy(), 1)
 
-plt.show()
+# plt.show()
 
 
 def data_iter(batch_size, features, labels):
     num_examples = len(features)
+    print('num_examples=', num_examples)
     indices = list(range(num_examples))  # indices在这里是一个向量
     """
     numbers=list(range(1,11))
@@ -34,20 +35,36 @@ def data_iter(batch_size, features, labels):
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     """
     # 这些样本是随机读取的，没有特定的顺序
+    print('打乱前：', indices)
     random.shuffle(indices)  # 随机打乱列表
-
+    print('打乱后：', indices)
     for i in range(0, num_examples, batch_size):  # range(start,stop[,step])    []代表不是必须
         batch_indices = torch.tensor(  # a = torch.tensor([1, 2, 3, 4, 5]) 创建向量
             indices[i:min(i + batch_size, num_examples)]
             # indices是一个向量
             # 获取向量的前三个元素可表示为：first_three_elements = v[0:3]
         )
-        yield features[batch_indices, labels[batch_indices]]
+        print('抽取：', batch_indices)
+        yield features[batch_indices], labels[batch_indices]
 
 
 batch_size = 10
-
 for X, y in data_iter(batch_size, features, labels):
     print(X, '\n', y)
     break
+
+# 3.2.3 初始化模型参数
+w = torch.normal(0, 0.01, size=(2,1), requires_grad=True)
+#  当requires_grad 设置为 True 时,表示这个张量的梯度将会被计算并存储在张量的 grad 属性中
+print('w=', w)
+b = torch.zeros(1, requires_grad=True)
+print('b=', b)
+
+
+# 3.2.4 定义模型
+def linreg(X, w, b):  #@save
+    """线性回归模型"""
+    return torch.matmul(X, w) + b
+
+
 
