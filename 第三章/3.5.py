db23016ff4de5ecabd286d06849d2149b925ec34
handsomekeai <lzllzlo@sina.com>
2024-06-24 11:48:29 +0800

    3.6

--- 第三章/3.5.py
+++ 第三章/3.5.py
@@ -1,5 +1,4 @@
 # 3.5 图像分类数据集
-
 import torch
 import torchvision
 from torch.utils import data
@@ -57,8 +56,53 @@ def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
 
 # 以下是训练数据集中前18个样本的图像及其相应的标签。
 X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
-print('X=', X, '\ny=', y)
-print('X[16]=', X[16])
-print('y[0]=', y[0])
+# print('X=', X, '\ny=', y)
+# print('X[16]=', X[16])
+# print('y[0]=', y[0])
+# X是图片，y是标签
 show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y))
-plt.show()
+# plt.show()
+
+# 3.5.2 读取小批量
+batch_size = 256
+
+
+def get_dataloader_workers():
+    """使用4个进程来读取数据"""
+    return 4
+
+
+train_iter = data.DataLoader(mnist_train, batch_size, shuffle=True,
+                             num_workers=get_dataloader_workers())
+timer = d2l.Timer()
+for X, y in train_iter:
+    continue
+print(f'{timer.stop():.2f} sec')
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    """下载Fashion-MNIST数据集，然后将其加载到内存中"""
+    trans = [transforms.ToTensor()]  # 规定数据会进行什么变换
+    if resize:  # 如果resize这个参数不为0我们就会重置它的分辨率
+        trans.insert(0, transforms.Resize(resize))  # 运行Resize函数，重置分辨率
+    trans = transforms.Compose(trans)  # 把这一系列的操作组合在一起
+    mnist_train = torchvision.datasets.FashionMNIST(  # 获取训练数据集
+        root="../data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(  # 获取测试数据集
+        root="../data", train=False, transform=trans, download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+# 对数据进行一系列的操作，
+# trans就可以理解为这些操作的一个集合
+# resize就是重新定义它的分辨率
+# 在pytorch中，我们的一切数据都需要分装成dataLoader，
+# 给定一个数据集，指定batch_size,再指定是否打乱，
+# 指定几个进程去读。这个都是用的很多的
+
+
+train_iter, test_iter = load_data_fashion_mnist(32, resize=64)
+for X, y in train_iter:
+    print(X.shape, X.dtype, y.shape, y.dtype)
+    break
